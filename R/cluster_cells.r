047d16b58695bed6379ba39ec8a62dcb64aa5234
Wuming Gong <gongx030@umn.edu>
2021-04-12 15:20:35 -0500

    update

--- R/cluster_cells.r
+++ R/cluster_cells.r
@@ -25,7 +25,16 @@ setMethod(
 
 		k <- ncol(reducedDim(x, reduction))
 
-		y <- as.Seurat(x, counts = 'counts', data = NULL) %>%
+		if (!is.null(assays(x)$counts) && !is.null(assays(x)$logcounts)){
+			y <- as.Seurat(x, counts = 'counts', data = 'logcounts')
+		}else if (is.null(assays(x)$counts) && !is.null(assays(x)$logcounts)){
+			y <- as.Seurat(x, counts = NULL, data = 'logcounts')
+		}else if (!is.null(assays(x)$counts) && is.null(assays(x)$logcounts)){
+			y <- as.Seurat(x, counts = 'counts', data = NULL)
+		}else
+			stop(sprintf('assays(x)$counts or assays(x)$logcounts cannot be NULL'))
+
+		y <- y %>%
 			FindNeighbors(reduction = reduction, k.param = method@n_neighbors, dims = 1:k, verbose = FALSE, graph.name = 'graph')
 
 		metadata(x)$snn <- as(y@graphs[['graph']], 'dgCMatrix')
@@ -36,7 +45,13 @@ setMethod(
 
 		cls <- lapply(res, function(r) FindClusters(y, algorithm = 3, resolution = r, verbose = FALSE, graph.name = 'graph') %>% Idents())
 		nmi <- sapply(1:length(cls), function(i) NMI(cls[[i]], colData(x)[[label]]))
-		colData(x)[[cluster]] <- cls[[which.max(nmi)]]
+
+		h <- which.max(nmi)
+		for (i in 1:length(res)){
+			sprintf('cluster_cells | nmi=%.3f | resolution=%.3e%s', nmi[i], res[i], ifelse(i == h, '*', '')) %>% message()
+		}
+
+		colData(x)[[cluster]] <- cls[[h]]
 		x
 	}
 )
